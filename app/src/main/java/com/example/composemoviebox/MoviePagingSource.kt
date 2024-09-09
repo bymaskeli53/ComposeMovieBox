3660a25838457fca99bfdf31e65b6003f7bd0f6e
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2024-09-09 12:59:20 +0300

    Create paging source

---
+++ app/src/main/java/com/example/composemoviebox/MoviePagingSource.kt
@@ -0,0 +1,31 @@
+package com.example.composemoviebox
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+
+class MoviePagingSource(
+    private val apiService: MovieService,
+    private val apiKey: String
+) : PagingSource<Int, Movie>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        return try {
+            val nextPage = params.key ?: 1
+            val response = apiService.getPopularMovies(apiKey,nextPage)
+            LoadResult.Page(
+                data = response.results, // List<Movie>
+                prevKey = if (nextPage == 1) null else nextPage - 1,
+                nextKey = if (response.results.isEmpty()) null else nextPage + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Movie>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+}
